3e6f7cd30a98e108ce4e729e9db696830c39b79d
glbter <nicknema13@gmail.com>
2021-02-08 22:41:44 +0200

    basic dao

---
+++ dao/Dao.cs
@@ -0,0 +1,37 @@
+﻿using Lab1.model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1.dao
+{
+    public class Dao<T> : IDao<T> where T : Base
+    {
+        private List<T> _entities;
+        public void Create(T entity)
+        {
+            _entities.Add(entity);
+        }
+
+        /*public void Delete(T entity)
+        {
+            _entities.Remove(entity);
+        }
+
+        public void Update(T entity)
+        {
+            Delete(entity);
+            Create(entity);
+        }*/
+
+        public T Get(Guid id)
+        {
+            return _entities.Find((T entity) => entity.Id.Equals(id));
+        }
+
+        public List<T> GetAll()
+        {
+            return _entities;
+        }
+    }
+}
---
+++ dao/DaoObject.cs
@@ -0,0 +1,13 @@
+﻿using Lab1.model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1.dao
+{
+    public class DaoObject
+    {
+        public IDao<Child> ChildDao { get; } = new Dao<Child>();
+        public IDao<Group> GroupDao { get; } = new Dao<Group>();
+    }
+}
