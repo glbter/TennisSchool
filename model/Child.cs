cc68bf48d17faa44226b4540094e7bf0ad6abb36
glbter <nicknema13@gmail.com>
2021-02-08 20:32:32 +0200

    created: basic app model

---
+++ model/Child.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1.model
+{
+    public class Child : Base
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public Group Group { get; set; }
+        private Guid _groupId;
+        public int Age 
+        {
+            get => (int) _birthday.Subtract(new DateTime()).TotalDays / 365; 
+        }
+        private DateTime _birthday;
+        public List<DayOfWeek> PreferableDays { get; private set; }
+        public GameLevel GameLevel { get; private set; }
+
+
+        public Child(string firstName, string lastName, 
+            GameLevel gameLevel, List<DayOfWeek> preferableDays, DateTime birthday )
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.GameLevel = gameLevel;
+            this.PreferableDays = preferableDays;
+            this._birthday = birthday;
+        }
+    }
+}
