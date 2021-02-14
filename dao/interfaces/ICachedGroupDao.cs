188367bd5ab1ce1aa70359a4f0741c70093ab14c
glbter <nicknema13@gmail.com>
2021-02-14 16:47:32 +0200

    add cached group, dao, moved interfaces

---
+++ dao/interfaces/ICachedGroupDao.cs
@@ -0,0 +1,13 @@
+﻿using Lab1.model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1.dao
+{
+    public interface ICachedGroupDao : IDao<CachedGroup>, IDaoUpdate<CachedGroup>, IDaoDelete<CachedGroup>
+    {
+        public List<CachedGroup> GroupsByGameLevel(GameLevel level);
+        public List<CachedGroup> GroupsByDayOfWeek(DayOfWeek dayOfWeek);
+    }
+}
---
+++ dao/interfaces/IDaoDelete.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1.dao
+{
+    public interface IDaoDelete<T>
+    {
+        public void Delete(T entity);
+    }
+}
