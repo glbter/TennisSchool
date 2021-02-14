cc9ac0d9da4fabe94a5b44350fd20909726f4f08
glbter <nicknema13@gmail.com>
2021-02-14 16:40:50 +0200

    add dao update, delete interface

---
+++ dao/IDaoUpdate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1.dao
+{
+    public interface IDaoUpdate<T>
+    {
+        public void Update(T entity);
+    }
+}
