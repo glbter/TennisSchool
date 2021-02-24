a7b1a24a6eabd8e0e97a1b619835987f8b7a2244
glbter <nicknema13@gmail.com>
2021-02-24 22:30:24 +0200

    moved Infrustructure into solution

---
+++ TennisClub.Infrastructure/services/ChildService.cs
@@ -0,0 +1,24 @@
+﻿using TennisClub.AppCore.model.impl;
+using TennisClub.AppCore.model.interfaces;
+using TennisClub.Data.dao;
+
+
+namespace TennisClub.Infrastructure.services
+{
+    public class ChildService
+    {
+        private readonly DaoObject dao;
+        public ChildService(DaoObject dao)
+        {
+            this.dao = dao;
+        }
+
+        public ICachedGroup SetChildToGroup(IChild child, ICachedGroup group)
+        {
+            child.GroupId = group.Id;
+            dao.ChildDao.Create((Child)child);
+            group.ChildrenAmount += 1;
+            return group;
+        }
+    }
+}
