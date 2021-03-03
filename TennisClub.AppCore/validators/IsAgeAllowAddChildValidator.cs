fc3d3f117aacc1b297f42924cfc0646503f62871
glbter <nicknema13@gmail.com>
2021-03-03 21:58:37 +0200

    fixed age filtering interval algorithm

--- TennisClub.AppCore/validators/IsAgeAllowAddChildValidator.cs
+++ TennisClub.AppCore/validators/IsAgeAllowAddChildValidator.cs
@@ -10,16 +10,16 @@ namespace TennisClub.AppCore.validators
         private readonly int maxAgeInterval;
         public IsAgeAllowAddChildValidator()
         {
-            this.maxAgeInterval = Convert.ToInt32(
-                    ConfigurationManager.AppSettings.Get("maxChildrenAgeIntervalInGroup"));
+            this.maxAgeInterval = 3; //Convert.ToInt32(
+            //ConfigurationManager.AppSettings.Get("maxChildrenAgeIntervalInGroup"));
         }
 
         public bool Validate(IChild<TK> child, ICachedGroup<TK> group)
         {
             int age = child.Age;
-            return (age >= group.MinAge && age <= group.MaxAge)
-                || age - group.MinAge <= maxAgeInterval
-                || group.MaxAge - age <= maxAgeInterval;
+            bool fitsInInterval = age >= group.MinAge && age <= group.MaxAge;
+            bool willFitInInterval = (age - group.MinAge <= maxAgeInterval) && (group.MaxAge - age <= maxAgeInterval);
+            return fitsInInterval || willFitInInterval;
         }
     }
 }
