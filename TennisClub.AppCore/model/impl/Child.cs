b9da7f64b91f0529399d74a5d2a278dd03f1fed6
glbter <nicknema13@gmail.com>
2021-03-03 19:40:01 +0200

    fixed id Guid() -> Guid.NewGuid() bug empty id

--- TennisClub.AppCore/model/impl/Child.cs
+++ TennisClub.AppCore/model/impl/Child.cs
@@ -1,25 +1,25 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 using TennisClub.AppCore.model.interfaces;
 
 namespace TennisClub.AppCore.model.impl
 {
     public class Child : IChild<Guid>
     {
-        public Guid Id { get; } = new Guid();
+        public Guid Id { get; }
         public string FirstName { get; }
         public string LastName { get; }
         public Guid GroupId { get; set; }
-        public int Age 
-        {
-            get => (int) birthday.Subtract(new DateTime()).TotalDays / 365; 
-        }
+        public int Age => DateTime.Today.Subtract(birthday).Days / 365;
+
         private DateTime birthday;
         public DayOfWeek PreferableDay { get; }
         public GameLevel GameLevel { get; }
 
         public Child(string firstName, string lastName, 
-            GameLevel gameLevel, DayOfWeek preferableDay, DateTime birthday )
+            GameLevel gameLevel, DayOfWeek preferableDay, DateTime birthday, Guid id = new Guid() )
         {
+            this.Id = (id == Guid.Empty) ? Guid.NewGuid() : id;
             this.FirstName = firstName;
             this.LastName = lastName;
             this.GameLevel = gameLevel;
--- TennisClub.Console/TennisClubProgram.cs
+++ TennisClub.Console/TennisClubProgram.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Configuration;
+using TennisClub.AppCore.model.interfaces;
 using TennisClub.Console.test;
 using TennisClub.Data.dao;
 using TennisClub.Infrastructure.pipelines;
@@ -15,6 +17,38 @@ namespace TennisClub.Console
 
             (new TestDataLoader()).InitTestData()
                 .ForEach(it => childLine.AddChild(it));
+
+            //dao.ChildDao.GetAll().ForEach(PrintChild);
+            System.Console.WriteLine("groups");
+            //dao.GroupDao.GetAll().ForEach(PrintGroup);
+            System.Console.WriteLine("cached groups");
+            //dao.CachedGroupDao.GetAll().ForEach(PrintCachedGroup);
+            System.Console.WriteLine(ConfigurationManager.GetSection("appSettings"));
+            var key = "maxAmountOfChildrenInGroup";
+            try  
+            {  
+                var appSettings = ConfigurationManager.AppSettings;  
+                string result = appSettings[key] ?? "Not Found";  
+                System.Console.WriteLine(result);  
+            }  
+            catch (ConfigurationErrorsException)  
+            {  
+                System.Console.WriteLine("Error reading app settings");  
+            }  
         } 
+        static void PrintChild(IChild<Guid> child)
+        {
+            System.Console.WriteLine($"{child.FirstName}, {child.LastName}, {child.Age.ToString()}");
+        }
+
+        static void PrintGroup(IGroup<Guid> group)
+        {
+           System.Console.WriteLine($"{group.GameLevel.ToString()}, {group.LessonsDay.ToString()} "); 
+        }
+        
+        static void PrintCachedGroup(ICachedGroup<Guid> group)
+        {
+            System.Console.WriteLine($"{group.ChildrenAmount.ToString()}, {group.MinAge.ToString()}, {group.MaxAge.ToString()} {group.Group.GameLevel} {group.Group.LessonsDay} "); 
+        }
     }
 }
