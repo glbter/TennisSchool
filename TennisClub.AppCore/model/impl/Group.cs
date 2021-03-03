39c0969d91d7413e30acd1d4b9ec30756b4b2ef6
glbter <nicknema13@gmail.com>
2021-03-03 19:40:01 +0200

    fixed id Guid() -> Guid.NewGuid() bug empty id

--- TennisClub.AppCore/model/impl/Group.cs
+++ TennisClub.AppCore/model/impl/Group.cs
@@ -5,12 +5,13 @@ namespace TennisClub.AppCore.model.impl
 {
     public class Group : IGroup<Guid>
     {
-        public Guid Id { get; } = new Guid();
+        public Guid Id { get; }
         public GameLevel GameLevel { get; }
         public DayOfWeek LessonsDay { get; }
 
-        public Group(GameLevel gameLevel, DayOfWeek lessonsDay)
+        public Group(GameLevel gameLevel, DayOfWeek lessonsDay, Guid id = new Guid())
         {
+            this.Id = (id == Guid.Empty) ? Guid.NewGuid() : id;
             this.GameLevel = gameLevel;
             this.LessonsDay = lessonsDay;
         }
