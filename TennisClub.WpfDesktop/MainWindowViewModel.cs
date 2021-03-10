d84f473c3a775f8e97c57362742fd38fde9a1598
glbter <nicknema13@gmail.com>
2021-03-10 19:11:16 +0200

    add relay RelayCommand, need to fix ambigious fields from prev commit

---
+++ TennisClub.WpfDesktop/MainWindowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TennisClub.WpfDesktop.model;
+using System.Windows.Input;
+
+
+namespace TennisClub.WpfDesktop
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Child> Children { get; private set; }
+        private Child _selectedChild { get; set; }
+        MainWindowViewModel()
+        {
+
+        }
+
+        private ICommand _addCommand;
+        public ICommand AddCommand
+        {
+            get => _addCommand ??= new RelayCommand<Child>(obj => 
+                {
+                   //do something  
+                });
+        }
+
+        public Child SelectedChild
+        {
+            get => _selectedChild;
+            set
+            {
+                _selectedChild = value;
+                OnPropertyChanged("SelectedChild");
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
