37853ea63887b914ebef4addc58484fa1b333780
glbter <nicknema13@gmail.com>
2021-03-15 21:29:54 +0200

    add bl to db mappers, and inverse for Child and Group

---
+++ TennisClub.Infrastructure/mappers/GroupToGroupInDbMapper.cs
@@ -0,0 +1,16 @@
+﻿using TennisClub.AppCore.model.impl;
+using TennisClub.Data.model;
+
+namespace TennisClub.Infrastructure.mappers
+{
+    public class GroupToGroupInDbMapper : IMapper<Group, GroupInDb>
+    {
+        public GroupInDb Map(Group entity)
+        {
+            return new GroupInDb(
+                id: entity.Id,
+                gameLevel: entity.GameLevel,
+                lessonsDay: entity.LessonsDay);
+        }
+    }
+}
---
+++ TennisClub.Infrastructure/mappers/IMapper.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace TennisClub.Infrastructure.mappers
+{
+    public interface IMapper<in TI, out TO>
+    {
+        public TO Map(TI entity);
+    }
+}
