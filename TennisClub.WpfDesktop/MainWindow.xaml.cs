ff0dae5eae48f2212fa6b8ce9f6ee36105f578f6
glbter <nicknema13@gmail.com>
2021-04-22 22:13:34 +0300

    update: working ui

--- TennisClub.WpfDesktop/MainWindow.xaml.cs
+++ TennisClub.WpfDesktop/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using TennisClub.WpfDesktop.model;
 
 
 namespace TennisClub.WpfDesktop
--- TennisClub.WpfDesktop/MainWindowViewModel.cs
+++ TennisClub.WpfDesktop/MainWindowViewModel.cs
@@ -9,7 +9,6 @@ using Microsoft.Extensions.DependencyInjection;
 using TennisClub.AppCore.model.impl;
 using TennisClub.Data.dao;
 using TennisClub.Infrastructure.interfaces;
-using TennisClub.Infrastructure.pipelines;
 using TennisClub.WpfDesktop.mapppers;
 using TennisClub.WpfDesktop.model;
 
@@ -21,12 +20,18 @@ namespace TennisClub.WpfDesktop
         private ChildWpf _newChild;
         private ChildWpf _prevChild;
         private bool _prevAdded;
+        private GroupWpf _chosenGroup;
+        private DayOfWeek _chosenDay;
         
         private ICommand _addCommand;
+        private ICommand _chooseGroupCommand;
+        private ICommand _addDayToListCommand;
         
         public ObservableCollection<ChildWpf> Children { get; private set; }
         public ObservableCollection<DayOfWeek> DaysOfWeek { get; private set; }
         public ObservableCollection<GameLevel> GameLevels { get; private set; }
+        public ObservableCollection<GroupWpf> GroupsToChoose { get; private set; }
+        public ObservableCollection<DayOfWeek> ChosenDays { get; private set; }
 
         private readonly IChildFacade _childFacade;
         private readonly IMapper<ChildWpf, Child> _fromUiChildMapper;
@@ -42,18 +47,16 @@ namespace TennisClub.WpfDesktop
             _fromUiGroupMapper = new FromUiGroupMapper();
             _toUiGroupMapper = new ToUiGroupMapper();
             IMapper<Child, ChildWpf> toUiChildMapper = new ToUiChildMapper();
-            
-            
-            // Children = new ObservableCollection<ChildWpf>(
-            //     _childFacade.GetAll()
-            //         .Select(toUiChildMapper.Map)
-            //         .ToList());
+
 
             DaysOfWeek = new ObservableCollection<DayOfWeek>(
                 Enum.GetValues(typeof(DayOfWeek)).OfType<DayOfWeek>());
-
+            Children = new ObservableCollection<ChildWpf>();
             GameLevels = new ObservableCollection<GameLevel>(
                 Enum.GetValues(typeof(GameLevel)).OfType<GameLevel>());
+            GroupsToChoose = new ObservableCollection<GroupWpf>();
+            ChosenDays = new ObservableCollection<DayOfWeek>();
+
         }
         
         public ICommand AddCommand
@@ -64,23 +67,62 @@ namespace TennisClub.WpfDesktop
                     _addCommand = new RelayCommand<ChildWpf>(obj =>
                     {
                         if (obj == null) return;
+                        obj.PreferableDays = ChosenDays.ToList();
                         List<GroupWpf> groups = _childFacade.AddChild(
                             _fromUiChildMapper.Map(obj))
                             .Select(_toUiGroupMapper.Map)
                             .ToList();
-                        if (groups.Count == 1) Children.Add(NewChild);
+                        if (groups.Count == 1)
+                        {
+                            Children.Add(NewChild);
+                            return;
+                        }
+                        else
+                        {
+                            groups.ForEach(GroupsToChoose.Add);
+                        }
                         _prevChild = _newChild;
+                        refreshDays();
                         NewChild = new ChildWpf("", "", GameLevel.Beginner, DayOfWeek.Sunday, DateTime.Today);
                     }
-                    // obj => notEmptyStr(obj?.Name) 
-                    //        && notEmptyStr(obj?.LastName)
-                    //        && obj.Birthday != null
                     );
                 }
                 return _addCommand;
             }
         }
+        
+        public ICommand ChooseGroupCommand
+        {
+            get
+            {
+                _chooseGroupCommand ??= new RelayCommand<GroupWpf>(obj =>
+                {
+                    if (obj == null) return;
+                    bool added = _childFacade.AddChildWithChosenGroup(
+                        _fromUiChildMapper.Map(_prevChild),
+                        _fromUiGroupMapper.Map(obj));
+                    if (added) Children.Add(NewChild);
+                    _prevChild = _newChild;
+                    NewChild = new ChildWpf("", "", GameLevel.Beginner, DayOfWeek.Sunday, DateTime.Today);
+                    GroupsToChoose.Clear();
+                });
+                return _chooseGroupCommand;
+            }
+        }
 
+        public ICommand AddDayToListCommand 
+        {
+            get
+            {
+                _addDayToListCommand ??= new RelayCommand<DayOfWeek>(obj =>
+                {
+                    ChosenDays.Add(obj);
+                    DaysOfWeek.Remove(obj);
+                    ChosenDay = DaysOfWeek.FirstOrDefault();
+                }, obj => !ChosenDays.Contains(obj));
+                return _addDayToListCommand;
+            }
+        }
         
         public ChildWpf NewChild
         {
@@ -91,12 +133,48 @@ namespace TennisClub.WpfDesktop
                 OnPropertyChanged("NewChild");
             }
         }
+        
+        public ChildWpf PrevChild
+        {
+            get => _prevChild;
+            set
+            {
+                _prevChild = value;
+                OnPropertyChanged("PrevChild");
+            }
+        }
+        
+        public GroupWpf ChosenGroup
+        {
+            get => _chosenGroup;
+            set
+            {
+                _chosenGroup = value;
+                OnPropertyChanged("ChosenGroup");
+            }
+        }
 
+        public DayOfWeek ChosenDay
+        {
+            get => _chosenDay;
+            set
+            {
+                _chosenDay = value;
+                OnPropertyChanged("ChosenDay");
+            }
+        }
+        
         private bool notEmptyStr(string str)
         {
             return str != String.Empty;
         }
 
+        private void refreshDays()
+        {
+            ChosenDays?.ToList().ForEach(DaysOfWeek.Add);
+            //ChosenDays?.ToList().ForEach(it => ChosenDays.Remove(it));
+            ChosenDays?.Clear();
+        }
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged([CallerMemberName] string prop = "")
         {
