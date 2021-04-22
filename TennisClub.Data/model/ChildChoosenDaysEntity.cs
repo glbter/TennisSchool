da95aff2e940844b71b0bcf9b44699460b4fa1e7
glbter <nicknema13@gmail.com>
2021-04-22 20:31:56 +0300

    fix: rename of attribute, added table, used sql migration

--- TennisClub.Data/model/ChildChoosenDaysEntity.cs
+++ TennisClub.Data/model/ChildChoosenDaysEntity.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TennisClub.Data.model
 {
     public class ChildChosenDaysEntity
     {
+        [Key]
         public Guid Id { get; set; }
+        [Required]
         public Guid ChildId { get; set; }
+        [Required]
         public DayOfWeek ChosenDay { get; set; }
 
         public ChildChosenDaysEntity(Guid childId, DayOfWeek chosenDay)
