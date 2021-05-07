da85ca9da3fec59fd9476655c0677243cb1f1e40
glbter <nicknema13@gmail.com>
2021-05-07 20:41:32 +0300

    add: main viewModel

---
+++ TennisClub.WpfDesktop/ViewModel/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using GalaSoft.MvvmLight.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TennisClub.WpfDesktop.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public INavigationService Navigation;
+        private ChildAddViewModel _childAddViewModel;
+
+        public MainViewModel(INavigationService navigation)
+        {
+            Navigation = navigation;
+        }
+        
+        public ChildAddViewModel ChildAddViewModel
+        {
+            get => _childAddViewModel;
+            set
+            {
+                _childAddViewModel = value;
+                RaisePropertyChanged(nameof(ChildAddViewModel));
+            }
+    }
+}
