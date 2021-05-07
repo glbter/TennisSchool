f1f3a96d17ad9dbb66cfb9167d0dee4da99068ab
glbter <nicknema13@gmail.com>
2021-05-07 20:37:58 +0300

    add: navigation service, view locator

---
+++ TennisClub.WpfDesktop/ViewModel/ViewLocator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Views;
+
+namespace TennisClub.WpfDesktop.ViewModel
+{
+    public class ViewLocator
+    {
+        private static Dictionary<ControlType, Lazy<UserControl>> controls =
+            new Dictionary<ControlType, Lazy<UserControl>>();
+
+        public const string ChildCredentialsPage = "ChildCredentialsPage";
+        
+        
+        public ViewLocator()
+        {
+            NavigationService navigationService = new NavigationService();
+            SimpleIoc.Default.Register<INavigationService>(() => navigationService);
+            
+            // navigationService.Configure
+            // controls.Add(ControlType.Accessories, new Lazy<UserControl>(() => new AccessoriesControl()));
+            // navigationService.Configure(LoginPage, new Uri("../Pages/Auth/LoginPage.xaml", UriKind.Relative));
+            
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        public MainViewModel MainViewModel 
+            => SimpleIoc.Default.GetInstance<MainViewModel>();
+
+        public static UserControl GetControl(ControlType controlType)
+        {
+            if (controls.ContainsKey(controlType))
+                return controls[controlType].Value;
+
+            return new UserControl();
+        }
+    }
+    
+    public enum ControlType
+    {
+        ChildCredentials
+    }
+}
