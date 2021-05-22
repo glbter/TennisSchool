3b3c16b310432d6dab3b3a4ed2f8a603783154c2
glbter <nicknema13@gmail.com>
2021-05-22 11:05:10 +0300

    fix: context factory for migrations

---
+++ TennisClub.Data/context/PostgresDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace TennisClub.Data.Context
+{
+    public class PostgresDbContextFactory : IDesignTimeDbContextFactory<PostgresDbContext>
+    {
+        public PostgresDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<PostgresDbContext>();
+            // TODO: move to config
+            string connectionString = "Host=localhost;Port=5432;Database=tennis-club;Username=postgres;Password=123";
+            optionsBuilder.UseNpgsql(connectionString);
+            return new PostgresDbContext(connectionString, optionsBuilder.Options);
+        }
+    }
+}
