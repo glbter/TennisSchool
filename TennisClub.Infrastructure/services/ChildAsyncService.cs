d4515d5cfc604f98d76f5f676e07ec25a2a45401
glbter <nicknema13@gmail.com>
2021-05-31 17:49:30 +0300

    add: async services

---
+++ TennisClub.Infrastructure/services/ChildAsyncService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using TennisClub.AppCore.Model.impl;
+using TennisClub.Data.Repository.interfaces;
+using TennisClub.Infrastructure.Interfaces;
+using TennisClub.Infrastructure.Mappers;
+
+namespace TennisClub.Infrastructure.Services
+{
+    public class ChildAsyncService : IChildAsyncService
+    { 
+        private readonly IUnitOfWork unitOfWork;
+        public ChildAsyncService(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task SetChildToGroupAsync(Child child, Group group)
+        {
+            child.GroupId = group.Id;
+            Task childTask = AddChildAsync(child);
+            Task groupTask = IncrementChildAmountInGroupAsync(group);
+            await Task.WhenAll(childTask, groupTask);
+        }
+
+        public async Task AddChildAsync(Child child)
+        {
+            Task createTask = unitOfWork.ChildRepository
+                .CreateAsync(new ChildToChildInDbMapper().Map(child));
+            
+            var days = child.PreferableDays
+                .Select(it => new ToChildDayMapper().Map(child, it))
+                .ToList();
+            
+            Task bulkInsertTask = unitOfWork.ChildChosenDaysRepository
+                .BulkInsertAsync(days);
+
+            await Task.WhenAll(createTask, bulkInsertTask);
+        }
+
+        private async Task IncrementChildAmountInGroupAsync(Group group)
+        {
+            var grp = await unitOfWork.GroupRepository.FindByIdAsync(group.Id);
+            grp.ChildrenAmount += 1;
+            unitOfWork.GroupRepository.Update(grp);
+        }
+    }
+}
---
+++ TennisClub.Infrastructure/services/GroupAsyncService.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using TennisClub.AppCore.Model.impl;
+using TennisClub.AppCore.Validators;
+using TennisClub.Data.Model;
+using TennisClub.Data.Repository.interfaces;
+using TennisClub.Infrastructure.Interfaces;
+using TennisClub.Infrastructure.Mappers;
+
+namespace TennisClub.Infrastructure.Services
+{
+    public class GroupAsyncService : IGroupAsyncService
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IChildAsyncService childService;
+        private readonly int maxChildren;
+        
+        public GroupAsyncService(IServiceProvider serviceProvider)
+        {
+            this.unitOfWork = serviceProvider.GetRequiredService<IUnitOfWork>();
+            this.childService = serviceProvider.GetRequiredService<IChildAsyncService>();
+            this.maxChildren = Convert.ToInt32(
+                ConfigurationManager.AppSettings["maxAmountOfChildrenInGroup"] ?? "5");
+        }
+
+        public async Task<List<Group>> TryAddChildToGroupAsync(Child child)
+        {
+            List<Group> groups = await FindVaccantGroupsAsync(child);
+            if (groups.Count <= 1)
+            {
+                Group group = groups.FirstOrDefault() 
+                              ?? CreateGroup(child.GameLevel, child.LessonsDay);
+                
+                if (groups.Count == 0) await ChooseDayAsync(child, group);
+                await unitOfWork.SaveChangesAsync();
+                await AddChildToGroupAsync(child, group);
+                groups = new List<Group> {group};
+            }
+            return groups;
+        }
+
+        public async Task AddChildToGroupAsync(Child child, Group group)
+        {
+            child.LessonsDay = group.LessonsDay;
+            await childService.SetChildToGroupAsync(child, group);
+            await unitOfWork.SaveChangesAsync();
+        }
+
+        private async Task<List<Group>> FindVaccantGroupsAsync(Child child)
+        {
+            List<GroupInDb> groupsFromDb = await unitOfWork.GroupRepository
+                    .FindVacantGroupsAsync(
+                        child.PreferableDays,
+                        child.GameLevel,
+                        maxChildren,
+                        new ChildAgeRuleChecker().CreateRuleCheckerDelegate(child));
+                
+            List<Group> groups = groupsFromDb
+                .Select(new GroupInDbToGroupMapper().Map)
+                .ToList();
+            return groups;
+        }
+
+        private Group CreateGroup(GameLevel gameLevel, DayOfWeek dayOfWeek)
+        {
+            Group group = new Group(gameLevel, dayOfWeek);
+            return group;
+        }
+
+        private async Task ChooseDayAsync(Child child, Group group)
+        {
+            var rand = new Random();
+            var index = rand.Next(child.PreferableDays.Count);
+            child.LessonsDay = child.PreferableDays[index];
+            group.LessonsDay = child.LessonsDay;
+            
+            await unitOfWork.GroupRepository.CreateAsync(
+                new GroupToGroupInDbMapper().Map(group));
+        }
+    }
+}
